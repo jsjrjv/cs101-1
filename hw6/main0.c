0e2d981c4a3f491cb43a923bacff851b378d8317
Yutingtingting <120004702+Yutingtingting@users.noreply.github.com>
2022-12-15 16:52:58 +0800

    Update main0.c

--- hw6/main0.c
+++ hw6/main0.c
@@ -3,10 +3,10 @@
 int main(){
     int i=5;
     if(i%2 == 0){
-    printf("%d is true\n",i);    
+        printf("%d is true\n",i);    
     }
     else{
-    printf("%d is false\n",i);
+        printf("%d is false\n",i);
     }
     return 0;
 }
